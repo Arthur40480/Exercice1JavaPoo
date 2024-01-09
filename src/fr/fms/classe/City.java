ba096dbc0fffc7e4250410df0ffc15e888278bb0
Arthur Gibert <arthurgibert@free.fr>
2024-01-09 11:28:27 +0100

    Ajout du constructeur City

--- src/fr/fms/classe/City.java
+++ src/fr/fms/classe/City.java
@@ -1,5 +1,15 @@
 package fr.fms.classe;
 
 public class City { // Classe City :
-
+	// Attributs :
+	public String cityName;			// Nom de la ville
+	public String country;			// Pays
+	public double nbrPeople;		// Nombre d'habitants
+	
+	// Constructeurs :
+	public City(String cityName, String country, double nbrPeople) {
+		this.cityName = cityName;
+		this.country = country;
+		this.nbrPeople = nbrPeople;
+	}
 }
