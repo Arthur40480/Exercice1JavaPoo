c177f11f909e810a95cd76235bb65fd531dc4ba5
Arthur Gibert <arthurgibert@free.fr>
2024-01-09 11:37:47 +0100

    Ajout d'une classe TestCity, et ajout de plusieurs instanciations de notre object City

---
+++ src/fr/fms/test/TestCity.java
@@ -0,0 +1,16 @@
+package fr.fms.test;
+
+import fr.fms.classe.*;
+
+public class TestCity {
+
+	public static void main(String[] args) {
+		
+		City toulouse = new City("Toulouse", "France", 450000);
+		City bangkok = new City("Bangkok", "Taïlande", 10000000);
+		City mostaganem = new City("Mostaganem", "Algérie", 300000);
+		City londres = new City("Londres", "Angleterre", 9000000);
+
+	}
+
+}
