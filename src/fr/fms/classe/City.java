750e4c2806fd85a4b15655bcb1c8e1ff73b6c6cf
Arthur Gibert <arthurgibert@free.fr>
2024-01-09 12:54:45 +0100

    Ajout des accesseurs

--- src/fr/fms/classe/City.java
+++ src/fr/fms/classe/City.java
@@ -1,15 +1,44 @@
 package fr.fms.classe;
+import java.util.*;
+
 
 public class City { // Classe City :
 	// Attributs :
 	public String cityName;			// Nom de la ville
 	public String country;			// Pays
-	public double nbrPeople;		// Nombre d'habitants
+	public int nbrPeople;		// Nombre d'habitants
 	
 	// Constructeurs :
-	public City(String cityName, String country, double nbrPeople) {
+	public City(String cityName, String country, int nbrPeople) {
 		this.cityName = cityName;
 		this.country = country;
+		this.nbrPeople = nbrPeople;
+	}
+	
+	// Accesseur (getter et setter) :
+	public String getCityName(String cityName) {
+		return cityName;
+	}
+	
+	public String getCountry(String country) {
+		return country;
+	}
+	
+	public int getNbrPeople(int nbrPeople) {
+		return nbrPeople;
+	}
+	
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public void setNbrPeople(int nbrPeople) {
+		if(nbrPeople < 0) 
+			throw new RunTimeException("Le nombre d'habitant ne peut être négatif !");
 		this.nbrPeople = nbrPeople;
 	}
 }
